599c7a02837c0b3cb25f47122f2ccabb5d289495
kibuikaCodes <kibuikasteve@gmail.com>
2022-02-15 14:11:40 +0300

    add api

---
+++ api/index.js
@@ -0,0 +1,16 @@
+
+// import express
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`api running on port ${port}`);
+})
+
+
