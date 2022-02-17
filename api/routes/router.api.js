61982987c2e42a3bec74d232fdbd886e7427e04f
john katua <johnkatua99@gmail.com>
2022-02-17 13:48:37 +0300

    Added routes get all vendors and single vendor by id

--- api/routes/router.api.js
+++ api/routes/router.api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sendEmail= require('../controllers/controller.sendEmails');
 const getallorders = require('../controllers/controller.getallorders');
 const getAllCustomers = require('../controllers/controller.getAllCustomers');
+const { getAllVendors, getSingleVendor } = require('../controllers/controllers.vendors');
 
 const apiRouter = express.Router();
 
@@ -9,6 +10,8 @@ const apiRouter = express.Router();
 apiRouter.route('/email').post(sendEmail); //send email
 apiRouter.route('/orders').get(getallorders) //get all orders & create order
 apiRouter.route('/customers').get(getAllCustomers) //get all customers;
+apiRouter.route('/vendors').get(getAllVendors) //get all vendors;
+apiRouter.route('/vendors/:id').get(getSingleVendor) //get single vendor;
 
 module.exports = apiRouter;
 
