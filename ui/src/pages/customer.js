caaae08e3124c1baea5e3ee4a4d22519346635a1
timsankara <chimpy.the.dev@outlook.com>
2022-02-17 16:11:12 +0300

    merge bernice to team2

--- ui/src/pages/customer.js
+++ ui/src/pages/customer.js
@@ -32,7 +32,7 @@ function Customer() {
   }
 
   useEffect(() => {
-    fetch("http://localhost:5010/api/customers")
+    fetch("https://team2-api-hackathon-2022.onrender.com/api/customers")
       .then(res => res.json())
       .then(data => {
         setAllCustomers(data)
