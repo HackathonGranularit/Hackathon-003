17c8a1c2ce8cd168f6bbb1f10ba04826750b01a4
Carver <kerenketepela@gmail.com>
2022-02-17 14:13:09 +0300

    vendors controller

--- api/controllers/vendors.js
+++ api/controllers/vendors.js
@@ -1,30 +1,20 @@
-const Vendor = require('../models/Vendor')
+const Vendor = require("../models/Vendor")
 
-const asyncHandler = require('../middleware/async');
+const asyncHandler = require("../middleware/async")
 
-
-module.exports.getVendors = asyncHandler(async (req,res,next) => {
-    const vendors = await Vendor.find();
-    res
-    .status(200)
-    .json({ count: vendors.length, data: vendors });
+module.exports.getVendors = asyncHandler(async (req, res, next) => {
+  const vendors = await Vendor.find()
+  res.status(200).json({ count: vendors.length, data: vendors })
 })
 
-module.exports.getVendorsById = asyncHandler(async (req,res,next) => {
-    const id = req.params.id
-    const vendors = await Vendor.findById(id).catch(() => {
-        res
-        .status(400)
-        .json({data:'Invalid id'})
-    })
+module.exports.getVendorsById = asyncHandler(async (req, res, next) => {
+  const id = req.params.id
+  const vendors = await Vendor.findById(id).catch(() => {
+    res.status(400).json({ data: "Invalid id" })
+  })
 
-    if(!customer){
-        res
-        .status(404)
-        .json({data: `Vendor with id:${id} not found`})
-    }
-    res
-    .status(200)
-    .json({count: 1, data: vendor})
-} )
-
+  if (!vendors) {
+    res.status(404).json({ data: `Vendor with id:${id} not found` })
+  }
+  res.status(200).json({ count: 1, data: vendors })
+})
