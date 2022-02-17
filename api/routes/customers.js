f7da0e13396d1fd93653f87ddbd5b26d7a66f0d5
Carver <kerenketepela@gmail.com>
2022-02-17 14:15:53 +0300

    customers route

--- api/routes/customers.js
+++ api/routes/customers.js
@@ -1,17 +1,14 @@
-const express = require('express')
-const { getCustomers, getCustomerById } = require('../controllers/customer')
-const {sendEmail} = require('../actions/sendEmail')
+const express = require("express")
+const { getCustomers, getCustomerById } = require("../controllers/customer")
+
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    getCustomers(req,res)
+router.get("/", (req, res) => {
+  getCustomers(req, res)
 })
 
-router.get('/:id', (req,res) => {
-    getCustomerById(req,res)
-})
-router.get('/mail/:id', (req,res) => {
-    sendEmail(req.res)
+router.get("/:id", (req, res) => {
+  getCustomerById(req, res)
 })
 
 // router.route('/:id').get(getCustomerById)
