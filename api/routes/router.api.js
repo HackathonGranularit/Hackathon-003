6e9e65a6ad3e4f8727d22717852469aa00056bda
mwai <mwaijohng@gmail.com>
2022-02-17 12:44:57 +0300

    added orders route

--- api/routes/router.api.js
+++ api/routes/router.api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { getAllCustomers } = require('../controllers/controller.getAllCustomers');
+const getallorders = require('../controllers/controller.getallorders')
 
 const apiRouter = express.Router();
 
 
 apiRouter.route('/customers').get(getAllCustomers);
+
+apiRouter.route('/orders').get(getallorders)
 
 
 module.exports = apiRouter;
